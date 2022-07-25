f4e38788a62cba5344e086fe9a96de91cfb5464a
Veids <veidsme@gmail.com>
2022-07-25 03:17:43 +0300

    track batteries

--- battery.lua
+++ battery.lua
@@ -2,37 +2,56 @@
 local lgi = require("lgi")
 local dbus = require("subsystem.dbus.lib")
 local Gio = lgi.Gio
+local gears = require("gears")
 
 local power = {}
 power.bus = dbus.SYSTEM
 power.batteries = {}
 
+local charger_is_tracked = false
+
 function power.emitChargerInfo(state)
   awesome.emit_signal("subsystem::charger", not state)
 end
 
-function power.emitBatteryInfo(batteryId, data)
-  awesome.emit_signal("subsystem::battery", batteryId, data)
+function power.emitBatteryInfo(updateType, devicePath, data)
+  awesome.emit_signal("subsystem::battery", updateType, devicePath, data)
 end
 
-function power.QueryInfo(data)
-  local objects = data[1]
-  for _, deviceId in ipairs(objects) do
-    local batteryName = deviceId:match("battery_(.+)")
-    if batteryName then
-      power.batteries[deviceId] = { trackers = {} }
+function power.QueryDevice(devicePath)
+  local batteryName = devicePath:match("battery_(.+)")
+  if batteryName then
+    power.batteries[devicePath] = { trackers = {} }
 
-      power.batteries[deviceId].trackers["UPower.Device"] = power.tracker(deviceId, "org.freedesktop.UPower.Device", function(data)
-        power.emitBatteryInfo(deviceId, data)
-      end)
-
-      power.batteries[deviceId].trackers["UPower"] = power.tracker("/org/freedesktop/UPower", "org.freedesktop.UPower", function(data)
+    if not charger_is_tracked then
+      power.batteries[devicePath].trackers["UPower"] = power.tracker("/org/freedesktop/UPower", "org.freedesktop.UPower", function(data)
         if data["OnBattery"] ~= nil then
           power.emitChargerInfo(data["OnBattery"])
         end
       end)
     end
+
+    power.batteries[devicePath].trackers["UPower.Device"] = power.tracker(devicePath, "org.freedesktop.UPower.Device", function(data)
+      power.emitBatteryInfo("update", devicePath, data)
+    end)
   end
+end
+
+function power.QueryDevices(data)
+  local objects = data[1]
+  for _, devicePath in ipairs(objects) do
+    power.QueryDevice(devicePath)
+  end
+end
+
+function power.DeviceAdded(conn, sender, devicePath, interface_name, signal_name, user_data)
+  local devicePath = dbus.unpackVariant(user_data)[1]
+  power.QueryDevice(devicePath)
+end
+
+function power.DeviceRemoved(conn, sender, _, interface_name, signal_name, user_data)
+  local devicePath = dbus.unpackVariant(user_data)[1]
+  power.emitBatteryInfo("remove", devicePath, nil)
 end
 
 power.tracker = dbus.BuildPropertyTracker(
@@ -51,5 +70,25 @@ power.bus:call(
   Gio.DBusCallFlags.NONE,
   -1,
   nil,
-  dbus.unwrap(power.QueryInfo)
+  dbus.unwrap(power.QueryDevices)
+)
+
+power.bus:signal_subscribe(
+  "org.freedesktop.UPower",
+  "org.freedesktop.UPower",
+  "DeviceAdded",
+  "/org/freedesktop/UPower",
+  nil,
+  Gio.DBusSignalFlags.NONE,
+  power.DeviceAdded
+)
+
+power.bus:signal_subscribe(
+  "org.freedesktop.UPower",
+  "org.freedesktop.UPower",
+  "DeviceRemoved",
+  "/org/freedesktop/UPower",
+  nil,
+  Gio.DBusSignalFlags.NONE,
+  power.DeviceRemoved
 )
